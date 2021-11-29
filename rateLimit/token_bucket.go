f7e2683e4f3cf9dd414938c62ec60ce25cdcc466
Chris Wojno <chris@wojno.com>
2021-11-29 08:57:47 -0800

    Fixed refactor typo.
    Added more docs.
    
    Removed initialize method and refactored tests.

--- rateLimit/token_bucket.go
+++ rateLimit/token_bucket.go
@@ -14,53 +14,59 @@ func defaultNow() time.Time {
 }
 
 type TokenBucketOpts struct {
-	// Capacity the maximum number of tokensAvailable the bucket may contain
+	// Capacity the maximum number of tokens the bucket may contain
 	Capacity uint64
-	// TokensAddedPerSecond how many tokensAvailable are replenished every second, tokensAvailable are added discretely but this may
-	// also be larger than 1 to add tokensAvailable more quickly than each second
+
+	// TokensAddedPerSecond how many tokens are replenished every second, tokens are added discretely but this may
+	// also be larger than 1 to add tokens more quickly than each second
 	TokensAddedPerSecond float64
+
 	// InitialTokens in the bucket. Prime the bucket
 	InitialTokens uint64
+
+	// nowFactory allows us to simulate time
+	nowFactory nowFactory
 }
 
 // TokenBucket is a rate-limiter using a token bucket scheme to approximate rates
 // Call NewTokenBucket to create a new instance with custom initialization.
-// Empty TokenBuckets contain no capacity and no tokensAvailable as well as no refill.
+// Empty TokenBuckets contain no capacity and no tokens as well as no refill.
 // Instance is _not_ thread-safe.
 type TokenBucket struct {
 	opts TokenBucketOpts
 
+	// tokensAvailable is the current number of tokens that can be consumed immediately
 	tokensAvailable uint64
+
 	// remainder is the left-over tokens generated thus far which would have been discarded because we round-down
 	// This ensures we don't lose fractionally generated tokens.
 	remainder   float64
 	lastUpdated time.Time
-
-	// nowFactory allows us to simulate time
-	nowFactory    nowFactory
-	isInitialized bool
 }
 
 func NewTokenBucket(opts TokenBucketOpts) *TokenBucket {
+	if opts.nowFactory == nil {
+		opts.nowFactory = defaultNow
+	}
 	return &TokenBucket{
 		opts:            opts,
 		tokensAvailable: opts.InitialTokens,
+		lastUpdated:     opts.nowFactory(),
 	}
 }
 
-// Allowed returns true only if tokenCost tokensAvailable are available. If the tokenCost is not available,
-// does not deduct the tokensAvailable and returns false
+// Allowed returns true only if tokenCost tokens are available. If the tokenCost is not available,
+// does not deduct the tokens and returns false
 func (b *TokenBucket) Allowed(tokenCost uint64) bool {
 	return b.allowed(tokenCost)
 }
 
 func (b *TokenBucket) allowed(cost uint64) bool {
-	b.initializeIfNeeded()
 	b.tokensAvailable, b.lastUpdated, b.remainder = replenishTokens(
 		b.tokensAvailable,
 		b.remainder,
 		b.lastUpdated,
-		b.nowFactory(),
+		b.opts.nowFactory(),
 		b.opts.TokensAddedPerSecond,
 		b.opts.Capacity)
 
@@ -71,23 +77,9 @@ func (b *TokenBucket) allowed(cost uint64) bool {
 	return true
 }
 
-// initializeIfNeeded will prepare the TokenBucket for use if NewTokenBucket was not called
-func (b *TokenBucket) initializeIfNeeded() {
-	if b.isInitialized {
-		return
-	}
-	b.isInitialized = true
-
-	if b.nowFactory == nil {
-		b.nowFactory = defaultNow
-	}
-
-	b.lastUpdated = b.nowFactory()
-}
-
-// replenishTokens adds tokensAvailable if any are available and there's capacity.
+// replenishTokens adds tokens if any are available and there's capacity.
 // Intended to be called each time allowed is called.
-// returns the new number of tokensAvailable and the new lastUpdated time
+// returns the new number of tokens and the new lastUpdated time
 func replenishTokens(tokens uint64,
 	remainder float64,
 	lastUpdated time.Time,
--- rateLimit/token_bucket_allowed_test.go
+++ rateLimit/token_bucket_allowed_test.go
@@ -17,7 +17,7 @@ var _ = Describe("TokenBucket.Allowed", func() {
 		})
 		When("does not refill", func() {
 			BeforeEach(func() {
-				bucket = &TokenBucket{}
+				bucket = NewTokenBucket(TokenBucketOpts{})
 			})
 			When("bucket has no tokensAvailable", func() {
 				It("rejects", func() {
@@ -25,13 +25,15 @@ var _ = Describe("TokenBucket.Allowed", func() {
 				})
 				When("time passes", func() {
 					BeforeEach(func() {
-						bucket.nowFactory = startAtAndAddDurations(
-							time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
-							10*time.Second,
-							1*time.Millisecond,
-							1*time.Millisecond,
-							1*time.Millisecond,
-						)
+						bucket = NewTokenBucket(TokenBucketOpts{
+							nowFactory: startAtAndAddDurations(
+								time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+								10*time.Second,
+								1*time.Millisecond,
+								1*time.Millisecond,
+								1*time.Millisecond,
+							),
+						})
 					})
 					It("does not refill", func() {
 						Expect(bucket.Allowed(unit)).Should(BeFalse())
@@ -44,7 +46,9 @@ var _ = Describe("TokenBucket.Allowed", func() {
 			})
 			When("bucket has tokensAvailable", func() {
 				BeforeEach(func() {
-					bucket.tokensAvailable = 2
+					bucket = NewTokenBucket(TokenBucketOpts{
+						InitialTokens: 2,
+					})
 				})
 				It("consumes them", func() {
 					Expect(bucket.Allowed(unit)).Should(BeTrue())
@@ -54,15 +58,19 @@ var _ = Describe("TokenBucket.Allowed", func() {
 			})
 		})
 		When("does refill", func() {
+			var (
+				opts TokenBucketOpts
+			)
 			BeforeEach(func() {
 				// used small numbers here to make examples easy to test
-				bucket = NewTokenBucket(TokenBucketOpts{
+				opts = TokenBucketOpts{
 					Capacity:             2,
 					TokensAddedPerSecond: 2.0,
 					InitialTokens:        1,
-				})
+				}
+				bucket = NewTokenBucket(opts)
 			})
-			When("bucket has no tokensAvailable", func() {
+			When("bucket has no tokens", func() {
 				BeforeEach(func() {
 					bucket.tokensAvailable = 0
 				})
@@ -71,38 +79,41 @@ var _ = Describe("TokenBucket.Allowed", func() {
 				})
 				When("enough time has passed to fill", func() {
 					BeforeEach(func() {
-						bucket.nowFactory = startAtAndAddDurations(
+						opts.nowFactory = startAtAndAddDurations(
 							time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
 							500*time.Millisecond,
 						)
+						bucket = NewTokenBucket(opts)
 					})
 					It("allows", func() {
 						Expect(bucket.Allowed(unit)).Should(BeTrue())
 					})
 				})
 			})
-			When("bucket has tokensAvailable", func() {
-				When("all tokensAvailable are expended", func() {
+			When("bucket has tokens", func() {
+				When("all tokens are expended", func() {
 					BeforeEach(func() {
-						bucket.nowFactory = startAtAndAddDurations(
+						opts.nowFactory = startAtAndAddDurations(
 							time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
 							1*time.Millisecond,
 							1*time.Millisecond,
 						)
+						bucket = NewTokenBucket(opts)
 					})
 					It("rejects", func() {
 						Expect(bucket.Allowed(unit)).Should(BeTrue())
 						Expect(bucket.Allowed(unit)).Should(BeFalse())
 					})
 				})
-				When("max tokensAvailable is reached", func() {
+				When("max tokens is reached", func() {
 					BeforeEach(func() {
-						bucket.nowFactory = startAtAndAddDurations(
+						opts.nowFactory = startAtAndAddDurations(
 							time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
 							10*time.Second,
 							1*time.Millisecond,
 							1*time.Millisecond,
 						)
+						bucket = NewTokenBucket(opts)
 					})
 					It("does not add more than Capacity", func() {
 						Expect(bucket.Allowed(unit)).Should(BeTrue())
@@ -111,13 +122,9 @@ var _ = Describe("TokenBucket.Allowed", func() {
 					})
 				})
 			})
-			When("called with fractionally-generated tokensAvailable", func() {
+			When("called with fractionally-generated tokens", func() {
 				BeforeEach(func() {
-					bucket.opts.Capacity = 5
-					bucket.opts.TokensAddedPerSecond = 10.0
-					bucket.opts.InitialTokens = 0
-					bucket.tokensAvailable = 0
-					bucket.nowFactory = startAtAndAddDurations(
+					opts.nowFactory = startAtAndAddDurations(
 						time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
 						125*time.Millisecond,
 						125*time.Millisecond,
@@ -125,8 +132,13 @@ var _ = Describe("TokenBucket.Allowed", func() {
 						125*time.Millisecond,
 						1*time.Millisecond,
 					)
+					bucket = NewTokenBucket(opts)
+					bucket.opts.Capacity = 5
+					bucket.opts.TokensAddedPerSecond = 10.0
+					bucket.opts.InitialTokens = 0
+					bucket.tokensAvailable = 0
 				})
-				It("does not lose fractions of tokensAvailable", func() {
+				It("does not lose fractions of tokens", func() {
 					_ = bucket.Allowed(unit)
 					_ = bucket.Allowed(unit)
 					_ = bucket.Allowed(unit)
@@ -136,8 +148,7 @@ var _ = Describe("TokenBucket.Allowed", func() {
 			})
 			When("initial tokens exceed capacity", func() {
 				BeforeEach(func() {
-					bucket.tokensAvailable = bucket.opts.Capacity * 2
-					bucket.nowFactory = startAtAndAddDurations(
+					opts.nowFactory = startAtAndAddDurations(
 						time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
 						1*time.Minute,
 						1*time.Millisecond,
@@ -145,6 +156,8 @@ var _ = Describe("TokenBucket.Allowed", func() {
 						1*time.Millisecond,
 						1*time.Millisecond,
 					)
+					bucket = NewTokenBucket(opts)
+					bucket.tokensAvailable = bucket.opts.Capacity * 2
 				})
 				It("does not overfill", func() {
 					Expect(bucket.Allowed(unit)).Should(BeTrue())
@@ -162,7 +175,7 @@ var _ = Describe("TokenBucket.Allowed", func() {
 			unit = 10
 		})
 
-		When("insufficient tokensAvailable", func() {
+		When("insufficient tokens", func() {
 			BeforeEach(func() {
 				bucket = NewTokenBucket(TokenBucketOpts{InitialTokens: unit / 2})
 			})
@@ -170,7 +183,7 @@ var _ = Describe("TokenBucket.Allowed", func() {
 				Expect(bucket.Allowed(unit)).Should(BeFalse())
 			})
 		})
-		When("sufficient tokensAvailable", func() {
+		When("sufficient tokens", func() {
 			BeforeEach(func() {
 				bucket = NewTokenBucket(TokenBucketOpts{InitialTokens: unit})
 			})
